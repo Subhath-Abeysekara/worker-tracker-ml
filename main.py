ad5716948dced82779e54a9d84496d8845c1c94a
Subhath Abeysekara <abeysekarasdp.19@uom.lk>
2022-10-10 01:23:46 +0530

    corsHandling

--- main.py
+++ main.py
@@ -1,54 +1,52 @@
 # This is a sample Python script
 import json
+
 import numpy as np
 import model as m
+#import Ml model
 import model2 as m2
 
+#import flask library for build apis
 from flask import Flask,request, jsonify
+#flask cors handing
+from flask_cors import CORS , cross_origin
+
+
+
 import pickle
 
 app = Flask(__name__)
+#enable cross origin for any ip
+CORS(app , resources={r"/":{"origins":"*"}})
 
-names = ["subhath","diniki"]
-
-model = pickle.load(open("model.pkl" , "rb"))
-
-projectIds = model[['title']].values.tolist()
-
-print(projectIds)
-
-np_array = model.to_numpy()
-l_list = model.values.tolist()
-
-print(np_array)
-print(l_list)
-
-
+#Home Api
 @app.route("/")
 def main():
     return "hello world"
 
+#First Page
 @app.route("/home")
+@cross_origin()
 def home():
-    return names
+    return "First Page"
 
+#Api for get suitable projects
 @app.route("/model" , methods = ["GET"])
+@cross_origin()
 def model():
+    #get rating value as api parameter
     id = request.args.get('id')
     print('id '+id)
-    students = [
-        {'name':"subhath" , 'age':24},
-        {'name':"diniki" , 'age':23}
-    ]
 
-    student = {'name':"subhath" , 'age':24}
-
+    #Get suitable five projects
     first_five_projects = m2.func1(int(id))
     print(first_five_projects)
+    #Listed projectIds to send response
     first_five_projects_list = first_five_projects['PrjecttId'].tolist()
-
+    #send response to api request
     return first_five_projects_list
 
 if __name__ == '__main__':
     app.debug = True
+    #init api port and ip address of the server
     app.run(host='0.0.0.0',port=5000)
--- model2.py
+++ model2.py
@@ -2,18 +2,23 @@ import pandas as pd
 import numpy as np
 import pickle
 
+#read the dataset
 df1 = pd.read_csv('/Users/DFT/Downloads/ratings_Beauty.csv')
+#get first 50 rows
 df2 = df1.head(50)
 print(df2)
 
 
 def func1(m):
+    #make parameter object
     options = {m}
+    #filter data rows acording to rating
     df3 = df2[df2['Rating'].isin(options)]
-    #df4 = df3['PrjecttId'].value_counts()
+    #Sort project ids acording to freaquence
     df4 = df3.groupby(['PrjecttId'])['Rating'].count().reset_index(
         name='Count').sort_values(['Count'], ascending=False)
     print(df4.head(5))
+    #return first five projects
     return df4.head(5)
 
 func1(5)
